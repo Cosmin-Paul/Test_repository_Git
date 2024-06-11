d2affd9f51ecd3047e281989673f3a0177055bb2
Cosmin <pcosmin_17@yahoo.com>
2024-06-11 21:03:35 +0300

    features added.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 40;
+        char initial= 'C';
+        double temp = 35.2d;
+        float heigh = 1.82f;
+        String firstName = "Cosmin";
+        String secondName = "Paul";
+        String colour = "Light Blue";
+
+        System.out.println("age:" + age);
+        System.out.println("initial:" + initial);
+        System.out.println("temp:" + temp);
+        System.out.println("heigh:" + heigh);
+        System.out.println("colour:" + colour);
+        System.out.println("My name is:" + firstName +  " " + secondName);
+
+        //new features added
     }
 }
